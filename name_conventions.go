a43696a2d43a1f7f7c0e21aa15b5acbfbcfa3f93
Monika-p-pixel <229728755+Monika-p-pixel@users.noreply.github.com>
2025-09-10 18:30:16 +0530

    name-convtn

---
+++ name_conventions.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+type employee struct {
+	firstname string
+	lastname  string
+	age	   int
+}
+func main() {
+	const Maxrities = 5
+	var employeeid int = 1001
+	var emp employee
+	emp.firstname = "John"
+	emp.lastname = "Doe"
+	emp.age = 30
+	fmt.Println("Employee ID:", employeeid)
+	fmt.Println("Employee Name:", emp.firstname, emp.lastname)
+	fmt.Println("Employee Age:", emp.age)
+
+	
+}
+
