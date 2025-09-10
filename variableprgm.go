23a04a00b084179dc632d213bd2fc40b6f5a7434
Monika-p-pixel <229728755+Monika-p-pixel@users.noreply.github.com>
2025-09-10 17:39:33 +0530

    go program

---
+++ variableprgm.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+func main(){
+	var a int = 10
+	var b string = "Hello"
+	var c float64 = 20.5
+	var d bool = true
+	var name string ="Alice"
+	middlename :="Bob"
+	lastname := "Smith"
+
+	fmt.Println("middlename:", middlename)
+
+
+	fmt.Println("Integer:", a)
+	fmt.Println("String:", b)
+	fmt.Println("Float:", c)
+	fmt.Println("Boolean:", d)
+
+
+
+}
+func Printname(){
+	firstname := "John"
+	fmt.Println("First name:", firstname)
+}
